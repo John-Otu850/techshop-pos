c147becdd0d09c1d17d315392c88eedc4267cef5
John Otu <otujohn.oj@gmail.com>
2026-01-08 22:43:48 +0000

    First of my POS

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+// 👇 REPLACE THE VALUES BELOW WITH YOUR SUPABASE CREDENTIALS
+
+const SUPABASE_URL = 'https://zxxgfmwyiwobwzwuovyf.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_4iPf_PhVHabhHyzP8JkQNQ_UrhcwjT4';
+
+// Initialize Supabase client
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
